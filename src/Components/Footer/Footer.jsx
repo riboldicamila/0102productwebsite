6f4a73debc1da71ed9e7c6af790e531b6912bd6c
riboldicamila <cami.riboldi@gmail.com>
2025-02-13 10:34:40 +0100

    content update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,46 +1,41 @@
-import "./Footer.css";
-import { FaTiktok, FaInstagram } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import logoImage from "../Images/logoWhite.png";
 import { aboutLinks, businessLinks, contactInfo } from "../../Data/FooterData";
+
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-column">
-        <img src={logoImage} alt="Logo" className="logo" />
-        <p className="text-light">Engineering user-friendly solutions.</p>
-      </div>
-      <div className="footer-column">
-        <h3 className="title">About Us</h3>
-        <ul>
-          {aboutLinks.map((link) => (
-            <li key={link.name}>
-              <Link to={link.path}>{link.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="footer-column">
-        <h3 className="title">Our Business</h3>
-        <ul>
-          {businessLinks.map((link) => (
-            <li key={link.name}>
-              <Link to={link.path}>{link.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="footer-column">
-        <h3 className="title">Contact Information</h3>
-        <p>
-          <strong>Location:</strong> {contactInfo.location}
-        </p>
-
-        <p>
-          <strong>Email:</strong>{" "}
-          <a href={`mailto:${contactInfo.email}`}>{contactInfo.email}</a>
-        </p>
+      <div className="footer-container">
+        <div className="footer-column">
+          <h3 className="title">About Us</h3>
+          <ul>
+            {aboutLinks.map((link) => (
+              <li key={link.name}>
+                <Link to={link.path}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h3 className="title">Our Business</h3>
+          <ul>
+            {businessLinks.map((link) => (
+              <li key={link.name}>
+                <Link to={link.path}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h3 className="title">Contact Information</h3>
+          <p>
+            <strong>Location:</strong> {contactInfo.location}
+          </p>
+          <p>
+            <strong>Tel:</strong> <a>{contactInfo.phone}</a>
+          </p>
+        </div>
       </div>
     </footer>
   );
--- src/Data/FooterData.js
+++ src/Data/FooterData.js
@@ -1,15 +1,15 @@
 export const aboutLinks = [
-  { name: "Camila", path: "" },
-  { name: "About", path: "/about" },
+  { name: "Pampa", path: "" },
+  { name: "History", path: "/about" },
 ];
 
 export const businessLinks = [
-  { name: "Technologies", path: "/technologies" },
-  { name: "Creations", path: "/about" },
-  { name: "Q&A", path: "/contact" },
+  { name: "FAQ", path: "/technologies" },
+  { name: "Shop", path: "/creations" },
+  { name: "Contact", path: "/contact" },
 ];
 
 export const contactInfo = {
-  location: "Worldwide. Argentina and Italy based.",
-  email: "cami.riboldi@gmail.com",
+  location: "123 Main Street, Anytown, USA",
+  phone: "+1 (555) 123-4567",
 };
