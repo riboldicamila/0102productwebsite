e2bbbbbc1ee7ce39903392d4e4aea4975424bb8f
riboldicamila <cami.riboldi@gmail.com>
2025-02-15 16:28:59 +0100

    content

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
-import { useState, useEffect } from 'react';
-import { FaEnvelope, FaGlobe, FaBars, FaTimes, FaShoppingCart } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';  
-import { navLinksLeft, navLinksRight } from '../../Data/NavbarData';
-import logoImage from '../Images/logo.png';
-import './Navbar.css';
+import { useState, useEffect } from "react";
+import {
+  FaEnvelope,
+  FaGlobe,
+  FaBars,
+  FaTimes,
+  FaShoppingCart,
+  FaShippingFast,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { navLinksLeft, navLinksRight } from "../../Data/NavbarData";
+import logoImage from "../Images/logo.png";
+import "./Navbar.css";
 
 function Navbar() {
   const [isSticky, setIsSticky] = useState(false);
@@ -12,24 +19,28 @@ function Navbar() {
 
   // Get cart state from Redux
   const cartItems = useSelector((state) => state.cart?.items);
-  const cartItemCount = cartItems?.reduce((total, item) => total + item.quantity, 0);
+  const cartItemCount = cartItems?.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
 
   const handleScroll = () => {
     setIsSticky(window.scrollY > 0);
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
     <div>
       <div className="top-bar">
-        <span>Free pickup in any of our stores - Free shipping on orders above £50</span>
+        <FaShippingFast size={30} />
+        <span> Free shipping on orders above £50</span>
       </div>
 
-      <nav className={`navbar ${isSticky ? 'sticky' : ''}`}>
+      <nav className={`navbar ${isSticky ? "sticky" : ""}`}>
         <button
           className="hamburger-menu"
           onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
@@ -41,7 +52,9 @@ function Navbar() {
           <img src={logoImage} alt="Logo" />
         </div>
 
-        <ul className={`nav-links ${isMobileMenuOpen ? 'mobile-menu-open' : ''}`}>
+        <ul
+          className={`nav-links ${isMobileMenuOpen ? "mobile-menu-open" : ""}`}
+        >
           {navLinksLeft.map((link) => (
             <li key={link.name}>
               <Link to={link.path}>{link.name}</Link>
